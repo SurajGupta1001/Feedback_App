202173643ce4e87c18d7c801479102f1f8e2484d
vikraj01 <99103720+vikraj01@users.noreply.github.com>
2023-02-07 19:15:40 +0530

    add

--- index.js
+++ index.js
@@ -2,7 +2,7 @@ const express = require("express")
 
 const app = express()
 
-// routing
+
 
 const obj = [
     {
@@ -19,25 +19,23 @@ app.get('/', (req, res) => {
     res.json(obj)
 })
 
+
+app.get('/html', (req,res) => {
+
+})
+
 app.get("/about", (req,res) => {
     res.send("About ")
 })
 
 app.get("/contact", (req,res) => {
 
-    // database fetching logic
-    // data
-
-    // json
-
-    // index.html
+    
 
     res.send("<h1>Contact</h1>")
 })
 
-// Methods
 
-// Midddleware
 
 
 app.listen(3000, () => console.log("Server started at http://localhost:3000"))
