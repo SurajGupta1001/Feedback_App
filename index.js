605c88438d7fcf6d013602bd267d580425f8f76e
vikraj01 <99103720+vikraj01@users.noreply.github.com>
2023-02-09 00:34:28 +0530

    message

--- index.js
+++ index.js
@@ -1,5 +1,5 @@
 const express = require("express")
-
+const mongoose = require("mongoose")
 const app = express()
 
 // Handle static files
@@ -8,19 +8,68 @@ app.use(express.static("public"))
 app.use(express.json())
 app.use(express.urlencoded({extended: false}))
 
+//--------------------------------------- Database -------------------------------//
+
+  // Define a schema for the feedback data
+  const feedbackSchema = new mongoose.Schema({
+    name: String,
+    email: String,
+    feedback: String,
+  });
+
+  // Create a model for the feedback data
+  const Feedback = mongoose.model("Feedback",feedbackSchema)
+  //------------------------------------------------------------------------------//
+
 
 app.get("/", (req,res, next) => {
     res.sendFile(`${__dirname}/public/feedback.html`)
 })
 
-app.post("/feedback", (req,res, next) => {
+
+app.get("/admin",(req,res,next) => {
+    res.sendFile(`${__dirname}/public/admin-login.html`)
+})
+
+app.post('/auth', (req,res,next) => {
+    const username = req.body.username;
+    const password = req.body.password;
+
+    if(username === "suraj" && password === "root"){
+        res.redirect('/feedback')
+    }
+
+    res.send("Unauthorized")
+})
+
+
+app.get('/feedback', async(req,res,next) =>{
+    const feedbacks = await Feedback.find();
+    return res.json(feedbacks)
+})
+
+app.post("/feedback",async (req,res, next) => {
     const name = req.body.username;
     const email =  req.body.email;
     const feedback = req.body.feedback;
 
+    const feed = new Feedback({
+        name: name,
+        email: email,
+        feedback: feedback
+    })
+
+    const savedFeed = await feed.save();
+
     console.log(name, email, feedback)
-    res.send("Your Feedback is accepted")
+    res.send(`<h1>${name} Feedback is accepted</h1>`)
 })
 
 
-app.listen(3000, () => console.log("Server started at http://localhost:3000"))
+const startServer = async() => {
+    await mongoose.connect("mongodb+srv://SurajGupta1001:root@cluster0.ntowqpv.mongodb.net/feedback")
+    console.log("Database Connected...")
+    app.listen(3000, () => console.log("Server started at http://localhost:3000"))
+}
+
+startServer();
