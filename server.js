2da3799c4f9bc0d570b2d7bcb7be5a0e4c158a59
vikraj01 <99103720+vikraj01@users.noreply.github.com>
2023-02-10 21:03:56 +0530

    add

---
+++ server.js
@@ -0,0 +1,54 @@
+const express = require("express")
+const mongoose = require("mongoose")
+const cookieParser = require("cookie-parser")
+
+
+const app = express()
+
+app.use(express.static("public"))
+
+app.use(express.json())
+app.use(express.urlencoded({extended: false}))
+app.use(cookieParser())
+
+
+const adminController = require("./controllers/admin.js")
+const feedbackController = require("./controllers/feedback")
+const authMiddleware = require("./middleware/authmiddleware")
+
+ 
+
+
+app.post("/logout", adminController.postLogout )
+app.get("/register",(req, res, next) => {
+    res.sendFile(`${__dirname}/public/register-admin.html`)
+})
+app.post("/register", adminController.postRegisterAdmin)
+app.get("/admin", (req,res,next) => {
+
+    if(req.cookies.isAuthenticated){
+        return res.redirect("/all-feedback")
+    }
+    res.sendFile(`${__dirname}/public/admin-login.html`)
+})
+app.post('/admin/login', adminController.postLoginAdmin)
+
+
+
+app.get("/", (req,res, next) => {
+    res.sendFile(`${__dirname}/public/feedback.html`)
+})
+app.get('/feedbacks', authMiddleware, feedbackController.getFeedbacks)
+app.get("/all-feedback", authMiddleware, (req,res,next) => {
+    res.sendFile(`${__dirname}/public/feedbacks.html`)
+})
+app.post("/feedback", feedbackController.postFeedback)
+
+
+const startServer = async() => {
+    await mongoose.connect("mongodb+srv://SurajGupta1001:root@cluster0.ntowqpv.mongodb.net/feedback")
+    console.log("Database Connected...")
+    app.listen(3000, () => console.log("Server started at http://localhost:3000"))
+}
+
+startServer();
